8d674d06168ea75e66e678019c6443a8688b135e
Aditya Raj Sapkota <aditya.sapkota@aits.local>
2021-08-19 03:58:48 +0545

    Completed the page across all screen sizes, All responsive updated, Performance updates and multiple more

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $(".primary-slider").owlCarousel({
+      dots: false,
+      margin: 60,
+      responsive:{
+        0:{
+            items:1.5,
+            margin: 16,
+        },
+        599: {
+          items: 3,
+          margin: 32,
+        },
+        1300:{
+            items: 3,
+        }
+      }
+    });
+  });
